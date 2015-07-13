981d7ea79bdbcbb7b842d471d6f2c72911432b8f
Matt Ficke <matthew.ficke@gmail.com>
2015-07-13 09:47:46 -0400

    script outline

---
+++ script.js
@@ -0,0 +1,11 @@
+//Create an array with all of the cards. Assume and infinite deck for version 1.
+
+//Click "play game" button, deal 2 cards each to player and dealer.
+
+//Calculate value of player's hand, display in relevant box. If player's hand is 21, player wins. If dealer's hand is 21, dealer wins. If both 21, push.
+
+//"Hit" button deals a new card to the player. Player Total is recalculated.
+
+// "Stay" button ends player's turn, dealer then takes cards according to the rules and the total is recalculated. Win, lose, or push is then calculated.
+
+//New game button is disaplayed.
