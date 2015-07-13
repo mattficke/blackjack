397094b92200de63c175ba44f7207d462a3bfbd2
Matt Ficke <matthew.ficke@gmail.com>
2015-07-13 10:29:06 -0400

    adding deal function

--- script.js
+++ script.js
@@ -9,3 +9,26 @@
 // "Stay" button ends player's turn, dealer then takes cards according to the rules and the total is recalculated. Win, lose, or push is then calculated.
 
 //New game button is disaplayed.
+
+
+//When "new game" button is clicked, randomly deal 2 cards face up to player, then one up and one down to dealer.
+var deal = function() {
+  var card;
+  var cardNum = Math.floor(Math.random() * 13 + 1);
+  if (cardNum === 1) {
+    card = "A";
+  }
+  else if (cardNum === 11) {
+    card = "J";
+  }
+  else if (cardNum === 12) {
+    card = "Q";
+  }
+  else if (cardNum === 13) {
+    card = "K";
+  }
+  else {
+    card = cardNum;
+  }
+  return card;
+}
